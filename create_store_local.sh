eba8a5323939d48817d6effabb143c5e8e87de02
Joao Fernandes <joaofernandesx@gmail.com>
2013-01-22 15:40:10 +0000

    scripts updated

---
+++ create_store_local.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Script for RoR website deployment on a local machine
+# Assumes that the store template is on github
+
+# Arguments:
+# $1 -> the store name
+# $2 -> the store id
+
+# The store directory
+STORE_NAME=$2"_"$1
+DIR="../plazr_stores/"$STORE_NAME
+LOG_FILE=$DIR"/log.txt"
+PORT=$((3000 + $2))
+DEFAULT_REPO="git@github.com:Plazr/plazr_store_template.git"
+
+#git clone $DEFAULT_REPO $DIR
+mkdir $DIR
+cp -R ../plazr_stores/plazr_store_template/* $DIR
+
+#LOGO_PATH=$DIR"/public/assets/"$2"/logo"
+#BANNER_PATH=$DIR"/public/assets/"$2"/banner"
+
+#mkdir -pv $LOGO_PATH
+#mkdir -pv $BANNER_PATH
+
+#plazr/public/assets/stores/ID
+#cp  "public/assets/images/"$2"/banner*" $BANNER_PATH
+#cp  "public/assets/stores/"$2"/logo*" $LOGO_PATH
+
+cd $DIR
+
+echo 'name: '$1 >> config/config.yml
+
+export BUNDLE_GEMFILE=$PWD"/Gemfile"
+
+#bundle install 
+#rake plazr_auth:install:migrations 
+#rake plazr_store:install:migrations 
+#rake db:migrate
+#rake db:seed
+
+rails server -d -e production -p $PORT
+
+echo
+echo "Setup of the $1 store is finished. Server is running in production on port $PORT"
+
--- create_store_old.sh
+++ create_store_old.sh
@@ -14,41 +14,33 @@
 STORE_NAME=$2"_"$1
 DIR="../plazr_stores/"$STORE_NAME
 NUM_DYNOS=1
-LOG_FILE="log.txt"
+LOG_FILE="../plazr_stores/"$DIR"/log.txt"
 DEFAULT_REPO="git@github.com:Plazr/plazr_store_template.git"
 
 echo "Progress log can be checked in $LOG_FILE"
 
-
-
-#git clone $DEFAULT_REPO $DIR >> $LOG_FILE
-mkdir $DIR
-cp -R ../plazr_stores/plazr_store_template/* $DIR
+git clone $DEFAULT_REPO $DIR > $LOG_FILE
 
 cd $DIR
-touch $LOG_FILE
 
-export BUNDLE_GEMFILE=$PWD"/Gemfile"
-
-bundle 
 # App creation on Heroku
 # Missing app name availability check
-heroku create $1 >> $LOG_FILE
+heroku create $1 > $LOG_FILE
 
 # Deploys the App on Heroku
-git push heroku master >> $LOG_FILE
+git push heroku master > $LOG_FILE
 
 # Scales the dyno run the webapp
-heroku ps:scale web=$NUM_DYNOS >> $LOG_FILE
+heroku ps:scale web=$NUM_DYNOS > $LOG_FILE
 
 # Adding a custom domain
 # Create subdomain on Gandi.net and associates it to heroku
 if [ ! -z "$3" ]
     then
-		heroku domains:add $3 >> $LOG_FILE
-  #  else
-		# aqui e que nao sei como sera
-		#heroku domains:add "www.plazr.net/store/"$2 > $LOG_FILE
+		heroku domains:add $3 > $LOG_FILE
+    else
+		
+		heroku domains:add "www.plazr.net/"$1 > $LOG_FILE
 fi
 
 echo "Setup of the $1 store is finished."
